a9e0b1631d24401acc5f96b5132659bf43f0293e
Garitskiy-Victor <dinomc4711@gmail.com>
2023-01-25 17:43:29 +0600

    Initial Commit

---
+++ Timer-Tomato-HW-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timer-Tomato-HW-12
+//
+//  Created by Victor Garitskyu on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
